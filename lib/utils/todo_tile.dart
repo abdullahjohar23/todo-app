5697b23372a37742bf47050dec14146d9a743f23
Abdullah Johar <jrrabdullah23@gmail.com>
2026-04-28 20:58:21 +0600

    tile design & functionalities created

---
+++ lib/utils/todo_tile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+    final String taskName;
+    final bool taskCompleted;
+    Function(bool?)? onChanged;
+
+    ToDoTile({
+        required this.taskName,
+        required this.taskCompleted,
+        required this.onChanged,
+        super.key,
+    });
+
+    @override
+    Widget build(BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.all(25.0),
+            child: Container(
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                    color: Colors.red[300],
+                    borderRadius: BorderRadius.circular(12),
+                ),
+                
+                child: Row(
+                    children: [
+                        // checkbox
+                        Checkbox(
+                            value: taskCompleted,
+                            onChanged: onChanged,
+                            activeColor: Colors.grey,
+                            autofocus: true,
+                        ),
+
+                        // task name
+                        Text(
+                            taskName,
+                        ),
+                    ],
+                ),
+            ),
+        );
+    }
+}
