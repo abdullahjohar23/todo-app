563c0a0621d2feee2557596a707cd76bb68d316e
Abdullah Johar <jrrabdullah23@gmail.com>
2026-04-29 13:08:22 +0600

    delelte function and Slidable function added

--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
     final String taskName;
     final bool taskCompleted;
     final Function(bool?)? onChanged;
+    final Function(BuildContext)? deleteFunction;
 
     const ToDoTile({
         required this.taskName,
         required this.taskCompleted,
         required this.onChanged,
+        required this.deleteFunction,
         super.key,
     });
 
@@ -16,34 +19,48 @@ class ToDoTile extends StatelessWidget {
     Widget build(BuildContext context) {
         return Padding(
           padding: const EdgeInsets.only(left: 25.0, right: 20.0, top: 25.0),
-            child: Container(
-                padding: const EdgeInsets.all(24),
-                decoration: BoxDecoration(
-                    color: Colors.red[300],
-                    borderRadius: BorderRadius.circular(12),
-                ),
-                
-                child: Row(
+            child: Slidable(
+                endActionPane: ActionPane(
+                    motion: StretchMotion(),
                     children: [
-                        // checkbox
-                        Checkbox(
-                            value: taskCompleted,
-                            onChanged: onChanged,
-                            activeColor: Colors.grey,
-                            autofocus: true,
+                        SlidableAction(
+                            onPressed: deleteFunction,
+                            icon: Icons.delete,
+                            backgroundColor: Colors.red[100]!,
+                            foregroundColor: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
                         ),
-
-                        // task name
-                        Text(
-                            taskName,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                decoration: taskCompleted
-                                    ? TextDecoration.lineThrough
-                                    : TextDecoration.none,
+                    ]
+                ),
+                child: Container(
+                    padding: const EdgeInsets.all(24),
+                    decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(12),
+                    ),
+                    
+                    child: Row(
+                        children: [
+                            // checkbox
+                            Checkbox(
+                                value: taskCompleted,
+                                onChanged: onChanged,
+                                activeColor: Colors.grey,
+                                autofocus: true,
                             ),
-                        ),
-                    ],
+                
+                            // task name
+                            Text(
+                                taskName,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    decoration: taskCompleted
+                                        ? TextDecoration.lineThrough
+                                        : TextDecoration.none,
+                                ),
+                            ),
+                        ],
+                    ),
                 ),
             ),
         );
